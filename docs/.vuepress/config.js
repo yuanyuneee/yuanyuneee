eff659b928b24ab72e48bc50ab8110c66ed1a92b
“yuanyuneee <Giyuanyuneee@163.com>
2020-06-29 23:37:29 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'Hello VuePress!',
+    description: 'Just playing around',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        {
+          text: 'Languages',
+          items: [
+            { text: 'Chinese', link: '/language/chinese' },
+            { text: 'English', link: '/language/english' }
+          ]
+        },
+        { text: 'External', link: 'https://www.baidu.com' },
+      ],
+      sidebarDepth: 2,
+      sidebar: [
+        ['/', '首页'],
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: ['/guide/']
+        }     
+      ]
+    }
+  }
